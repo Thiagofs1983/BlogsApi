7238b001182c893b83ff39b0dfe37af043d9c60d
Thiago Fonseca de Souza <t.fs.2007@hotmail.com>
2022-08-26 07:19:15 -0300

    feat: model Category que faz referência a tabela Categories

---
+++ src/database/models/category.js
@@ -0,0 +1,18 @@
+module.exports = (sequelize, DataTypes) => {
+    const Category = sequelize.define('Category', {
+      id: { type: DataTypes.INTEGER, primaryKey: true },
+      name: DataTypes.STRING,
+    },
+    {
+      timestamps: false,
+      tableName: 'Categories'
+    });
+  
+    /* Category.associate = (models) => {
+      Category.hasMany(models.PostCategory, {
+        foreignKey: 'userId', as: 'BlogPosts' 
+      });
+    } */
+  
+    return Category;
+  }
