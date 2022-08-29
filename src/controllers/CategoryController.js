632faa5269eb5eb80e8a5de7c9f1d5f3e1b321f7
Thiago Fonseca de Souza <t.fs.2007@hotmail.com>
2022-08-29 17:00:24 -0300

    fix: refatora camada de controllers para Category com condicional try/catch

--- src/controllers/CategoryController.js
+++ src/controllers/CategoryController.js
@@ -1,14 +1,24 @@
 const CategoryService = require('../services/CategoryService');
 
+const SERVER_ERROR = { message: 'Algo de errado no servidor' };
+
 const createCategory = async (req, res) => {
-  const name = req.body;
-  const result = await CategoryService.createCategory(name);
-  res.status(201).json(result);
+  try {
+    const name = req.body;
+    const result = await CategoryService.createCategory(name);
+    res.status(201).json(result);
+  } catch (error) {
+    res.status(500).json(SERVER_ERROR);
+  }
 };
 
 const getAll = async (req, res) => {
-  const result = await CategoryService.getAll();
-  res.status(200).json(result);
+  try {
+    const result = await CategoryService.getAll();
+    res.status(200).json(result);
+  } catch (error) {
+    res.status(500).json(SERVER_ERROR);
+  }
 };
 
 module.exports = {
