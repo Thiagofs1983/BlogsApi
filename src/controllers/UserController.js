af24bca0d7e2bfe5e87f10e00f6dcf03d2287368
Thiago Fonseca de Souza <t.fs.2007@hotmail.com>
2022-08-29 16:51:09 -0300

    fix: refatora camada de controllers para Users com condicional try/catch

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -1,34 +1,56 @@
 const UserService = require('../services/UserService');
 
+const SERVER_ERROR = { message: 'Algo de errado no servidor' };
+
 const login = async (req, res) => {
-  const { email, password } = req.body;
-  const token = await UserService.login({ email, password });
-  if (!token) return res.status(400).json({ message: 'Invalid fields' });
-  res.status(200).json({ token });
+  try {
+    const { email, password } = req.body;
+    const token = await UserService.login({ email, password });
+    if (!token) return res.status(400).json({ message: 'Invalid fields' });
+    res.status(200).json({ token });
+  } catch (error) {
+    res.status(500).json(SERVER_ERROR);
+  }
 };
 
 const create = async (req, res) => {
-  const token = await UserService.create(req.body);
-  if (!token) return res.status(409).json({ message: 'User already registered' });
-  res.status(201).json({ token });
+  try {
+    const token = await UserService.create(req.body);
+    if (!token) return res.status(409).json({ message: 'User already registered' });
+    res.status(201).json({ token });
+  } catch (error) {
+    res.status(500).json(SERVER_ERROR);
+  }
 };
 
 const getUser = async (req, res) => {
-  const users = await UserService.getUser();
-  res.status(200).json(users);
+  try {
+    const users = await UserService.getUser();
+    res.status(200).json(users);
+  } catch (error) {
+    res.status(500).json(SERVER_ERROR);
+  }
 };
 
 const getUserById = async (req, res) => {
-  const { id } = req.params;
-  const result = await UserService.getUserById(id);
-  if (!result) return res.status(404).json({ message: 'User does not exist' });
-  res.status(200).json(result);
+  try {
+    const { id } = req.params;
+    const result = await UserService.getUserById(id);
+    if (!result) return res.status(404).json({ message: 'User does not exist' });
+    res.status(200).json(result);
+  } catch (error) {
+    res.status(500).json(SERVER_ERROR);
+  }
 };
 
 const removeUser = async (req, res) => {
-  const email = req.user;
-  await UserService.removeUser(email);
-  res.status(204).end();
+  try {
+    const email = req.user;
+    await UserService.removeUser(email);
+    res.status(204).end();
+  } catch (error) {
+    res.status(500).json(SERVER_ERROR);
+  }
 };
 
 module.exports = {
