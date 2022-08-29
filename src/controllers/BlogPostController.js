e788327d536af5d35d536211237506ebfdec02fc
Thiago Fonseca de Souza <t.fs.2007@hotmail.com>
2022-08-29 17:01:13 -0300

    fix: refatora camada de controllers para BlogPost com condicional try/catch

--- src/controllers/BlogPostController.js
+++ src/controllers/BlogPostController.js
@@ -1,47 +1,73 @@
 const BlogPostService = require('../services/BlogPostService');
 
+const SERVER_ERROR = { message: 'Algo de errado no servidor' };
+
 const getAllBlogPost = async (req, res) => {
-  const result = await BlogPostService.getAllBlogPost();
-  res.status(200).json(result);
+  try {
+    const result = await BlogPostService.getAllBlogPost();
+    res.status(200).json(result);  
+  } catch (error) {
+    res.status(500).json(SERVER_ERROR);
+  }
 };
 
 const getPostById = async (req, res) => {
-  const { id } = req.params;
-  const result = await BlogPostService.getPostById(id);
-  if (!result) return res.status(404).json({ message: 'Post does not exist' });
-  res.status(200).json(result);
+  try {
+    const { id } = req.params;
+    const result = await BlogPostService.getPostById(id);
+    if (!result) return res.status(404).json({ message: 'Post does not exist' });
+    res.status(200).json(result);  
+  } catch (error) {
+    res.status(500).json(SERVER_ERROR);
+  }
 };
 
 const createPost = async (req, res) => {
-  const emailUser = req.user;
-  const { title, content, categoryIds } = req.body;
-  const result = await BlogPostService.createPost({ title, content, categoryIds }, emailUser);
-  if (!result) return res.status(400).json({ message: '"categoryIds" not found' });
-  res.status(201).json(result);
+  try {
+    const emailUser = req.user;
+    const { title, content, categoryIds } = req.body;
+    const result = await BlogPostService.createPost({ title, content, categoryIds }, emailUser);
+    if (!result) return res.status(400).json({ message: '"categoryIds" not found' });
+    res.status(201).json(result);  
+  } catch (error) {
+    res.status(500).json(SERVER_ERROR);
+  }
 };
 
 const updatePost = async (req, res) => {
-  const { id } = req.params;
-  const email = req.user;
-  const result = await BlogPostService.updatePost(req.body, id, email);
-  if (!result) return res.status(401).json({ message: 'Unauthorized user' });
-  res.status(200).json(result);
+  try {
+    const { id } = req.params;
+    const email = req.user;
+    const result = await BlogPostService.updatePost(req.body, id, email);
+    if (!result) return res.status(401).json({ message: 'Unauthorized user' });
+    res.status(200).json(result);  
+  } catch (error) {
+    res.status(500).json(SERVER_ERROR);
+  }
 };
 
 const removePost = async (req, res) => {
-  const { id } = req.params;
-  const email = req.user;
-  const result = await BlogPostService.removePost(id, email);
-  if (result === null) return res.status(404).json({ message: 'Post does not exist' });
-  if (result === 0) return res.status(401).json({ message: 'Unauthorized user' });
-  res.status(204).end();
+  try {
+    const { id } = req.params;
+    const email = req.user;
+    const result = await BlogPostService.removePost(id, email);
+    if (result === null) return res.status(404).json({ message: 'Post does not exist' });
+    if (result === 0) return res.status(401).json({ message: 'Unauthorized user' });
+    res.status(204).end();
+  } catch (error) {
+    res.status(500).json(SERVER_ERROR);
+  }
 };
 
 const searchPost = async (req, res) => {
-  const { q } = req.query;
-  const result = await BlogPostService.searchPost(q);
-  console.log(result);
-  res.status(200).json(result);
+  try {
+    const { q } = req.query;
+    const result = await BlogPostService.searchPost(q);
+    console.log(result);
+    res.status(200).json(result);
+  } catch (error) {
+    res.status(500).json(SERVER_ERROR);
+  }
 };
 
 module.exports = {
